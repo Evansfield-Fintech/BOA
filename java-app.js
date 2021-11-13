edb33a2181a1120a70f14c747d4785fa8cc905c6
Osazee Onaghinor <osazeeoj@gmail.com>
2021-11-13 01:07:06 +0000

    first mod

--- java-app.js
+++ java-app.js
@@ -1,3 +1,5 @@
 Welcome to DevOps Training
 Now, It would be interesting o see what the next sets of modules would bring.
 There were some challenging sections in the shell scripting module. However, with patience, I learnt the various commands for managing condition (if else and switch cases), load tests (using for-loop and while-loop), and funtions that contain nested if arguments.
+
+With patience and dedication, we shall excel.
