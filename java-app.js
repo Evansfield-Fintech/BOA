3754edac847b2b9c040a6ad86ed22391573b0018
Osazee Onaghinor <osazeeoj@gmail.com>
2021-11-13 01:08:44 +0000

    second mod

--- java-app.js
+++ java-app.js
@@ -3,3 +3,5 @@ Now, It would be interesting o see what the next sets of modules would bring.
 There were some challenging sections in the shell scripting module. However, with patience, I learnt the various commands for managing condition (if else and switch cases), load tests (using for-loop and while-loop), and funtions that contain nested if arguments.
 
 With patience and dedication, we shall excel.
+
+With Jehovah by my side, I will not run and hide, My God is with me. This, I know!
