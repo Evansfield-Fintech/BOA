9e3f9b9879a774c9377645dfb7cc77cd89b130d2
Osazee Onaghinor <osazeeoj@gmail.com>
2021-11-13 01:07:06 +0000

    first mod

--- java-app.js
+++ java-app.js
@@ -3,3 +3,5 @@ DevOps is good. It makes the mind work smarter.
 This module on github branching makes for a lot of reflection to understand the concept.
 Now, It would be interesting to see what the next sets of modules would bring.
 There were some challenging sections in the shell scripting module. However, with patience, I learnt the various commands for managing condition (if else and switch cases), load tests (using for-loop and while-loop), and funtions that contain nested if arguments.
+
+With patience and dedication, we shall excel.
