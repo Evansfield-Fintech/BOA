013e8b2e888d96f82a9feb527e6c694842e3eb32
Osazee Onaghinor <osazeeoj@gmail.com>
2021-11-13 02:57:05 +0000

    Revert "update verb"
    This reverts commit 95235f90982d747af250518a39e758a8914e9431.
    
    reversing updates to verb

--- java-app.js
+++ java-app.js
@@ -2,7 +2,7 @@ Welcome to DevOps Training.
 DevOps is good. It makes the mind work smarter.
 This module on github branching makes for a lot of reflection to understand the concept.
 Now, It would be interesting to see what the next sets of modules would bring.
-There were some difficult sections in the shell scripting module. However, with patience, I learnt the various commands for managing condition (if else and switch cases), load tests (using for-loop and while-loop), and funtions that contain nested if arguments.
+There were some challenging sections in the shell scripting module. However, with patience, I learnt the various commands for managing condition (if else and switch cases), load tests (using for-loop and while-loop), and funtions that contain nested if arguments.
 
 With patience and dedication, we shall excel.
 
